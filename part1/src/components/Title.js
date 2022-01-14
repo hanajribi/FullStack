ce628cdf81feaf79ac33226d51a1a552f056bbf2
hanajribi <jribihana2@gmail.com>
2022-01-14 19:25:41 +0100

    first commit

---
+++ part1/src/components/Title.js
@@ -0,0 +1,10 @@
+const Title = (props) => {
+    return (
+      <div>
+        <p>
+          Hello {props.name}, you are {props.age} years old
+        </p>
+      </div>
+    )
+  }
+  export default Title;
