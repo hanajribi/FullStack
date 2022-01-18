be29f50bb4f8d037a0a277c44267b0eae204ea27
hanajribi <jribihana2@gmail.com>
2022-01-18 21:00:17 +0100

    second commit

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,73 +1,49 @@
 
-
 import React from 'react'
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.course}
-      </h1>
-    </div>
-  )
-}
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
 }
 const Total = (props) => {
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  const tab = [];
+  props.parts.map(part => tab.push(part.exercises))
+  const s = tab.reduce(reducer);
+  console.log(tab)
   return (
     <div>
-      <p>
-      Number of exercises {props.total}
-      </p>
+      <p>Number of exercises {s}</p>
     </div>
   )
 }
 const Content = (props) => {
- 
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return (
     <div>
-      <p>
-       <Part part={part1} exercises={exercises1}/>
-       <Part part={part2} exercises={exercises2}/>
-       <Part part={part3} exercises={exercises3}/>
-      </p>
+      {props.parts.map(value => <h5> {value.name} {value.exercises}</h5>)}
+
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const exercises1 = 10
- 
-  const exercises2 = 7
-
-  const exercises3 = 14
-
-  
 
   return (
-    
-     <div>
-     <Header course={course} />
-     <Content  />
-     <Total total={exercises1 + exercises2 + exercises3} />
-   </div>
+
+    <div>
+      
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} /> 
+
+    </div>
   )
 }
 
 export default App
+
+
+
+
+
